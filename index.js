45d9c61b903339a80ff1f29e1e1e0f97e2c927d1
Alessandro Menezes <alessandroadm@live.com>
2020-02-03 15:14:45 -0300

    Adicionado arrow function

--- index.js
+++ index.js
@@ -1,2 +1,21 @@
-const series = ['Game of Thrones', 'Breaking Bad', 'Lost in space'];
+let series = ['Game of Thrones', 'Breaking Bad', 'Lost in space'];
+let imprimirSeries = series => {
+  for (let serie of series) {
+    console.log(serie);
+  }
+};
 
+let filme = {
+  duracao: 80,
+  tipo: 'Ficção',
+  diretor: 'Steven Spelberg',
+  ano: 2020
+};
+
+let imprimirFilme = filme => {
+  for (let prop in filme) {
+    console.log(filme[prop]);
+  }
+};
+
+imprimirFilme(filme);
